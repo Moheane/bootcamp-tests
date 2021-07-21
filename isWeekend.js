15934f9874844d8e0293d372fbeb15e937569722
Ramahadi Jan <moheanerj@gmail.com>
2021-07-21 09:15:22 +0200

    imported missing isWeekend code function from codex

---
+++ isWeekend.js
@@ -0,0 +1,11 @@
+function isWeekday(day){
+    var isWeekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    var isWeekend = ["Saturday", "Sunday"];
+      
+      if(isWeekday.includes(day)){
+          return true
+      }
+      else{
+      return false
+      }
+    }
