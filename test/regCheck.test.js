a6681a758db88e02639883ee1caec75e3d86fc7c
Ramahadi Jan <moheanerj@gmail.com>
2021-07-21 14:44:04 +0200

    fixed regCheck test

--- test/regCheck.test.js
+++ test/regCheck.test.js
@@ -2,7 +2,7 @@ describe('RegCheck Test', function(){
     it('should return "true" if "Ramahadi" ends with "i" when given "Ramahadi, a" as arguments', function(){
         assert.equal(true, regCheck('Ramahadi', 'i'));
     });
-    it('should return "false" if "Ramahadi" does not ends with "s" when given "Ramahadi, a"', function(){
+    it('should return "false" if "Ramahadi" does not ends with "s" when given "Ramahadi, s"', function(){
         assert.equal(false, regCheck('Ramahadi', 's'));
     });
 
