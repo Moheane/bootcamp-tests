9e1ae03797487f6d1944c5faf8e9c9d68b5a7dda
Ramahadi Jan <moheanerj@gmail.com>
2021-07-21 11:14:49 +0200

    completed countregNumber test

--- test/countRegNumber.test.js
+++ test/countRegNumber.test.js
@@ -1,8 +1,11 @@
-descibe('countRegNumber test', function(){
+describe('countRegNumber test', function(){
     it('should return total 3 when given "I, am, Ramahadi" as an argument', function(){
-        assert.equal(3, 'I, am, Ramahadi')
+        assert.equal(3, countRegNumber('I, am, Ramahadi'))
     });
-    it('should return total 0 when given no argument', function(){
-        assert.equal(0, 'I, am, Ramahadi')
+    it('should return total 7 when given "This, is, a, count, register, number, funtion" as an argument', function(){
+        assert.equal(7, countRegNumber('This, is, a, count, register, number, funtion" as an argument'))
+    });
+    it('should return total 1 when given no argument', function(){
+        assert.equal(1,countRegNumber(''))
     })
 })
