9264776a7c7a422b498e66b61bb5c71b42edbf81
Ramahadi Jan <moheanerj@gmail.com>
2021-07-21 07:40:48 +0200

    fixed and finalized  the greet test

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,6 +1,12 @@
 describe('this test' , function(){
-    it('Should return "Hello, Ramahadi"' , function(){
-        assert.equal('Hello, Ramahadi','Ramahadi');
+    it('should return "Hello, Ramahadi" when greeting Ramahadi' , function(){
+        assert.equal('Hello, Lesego', greet('Ramahadi'));
+
+    });
+ 
+    it('should just return "Hello," when given no name' , function(){
+        assert.equal('Hello', greet());
+
     });
 
 });
